b1a7876ad86fbcbe57910f5d7224861fe67566da
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-07-19 23:04:51 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import React, { useState } from "react";
 
-ReactDOM.render(
-  <Form />,
-  document.getElementById('root')
-);
+function Form() {
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry",
+    admin: false,
+  });
+
+  function handleChange(event) {
+    const name = event.target.name;
+    let value = event.target.value;
+
+    // use `checked` property of checkboxes instead of `value`
+    if (event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="firstName"
+        onChange={handleChange}
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        name="lastName"
+        onChange={handleChange}
+        value={formData.lastName}
+      />
+      <input
+        type="checkbox"
+        name="admin"
+        onChange={handleChange}
+        checked={formData.admin}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+
+
+export default Form;
